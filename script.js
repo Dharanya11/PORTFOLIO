aefe78b0484a8c4c5d554c34ab3855008c0d88cf
Dharanya A <dharanyaa.23it@kongu.edu>
2026-02-13 22:37:30 +0530

    updated partially

--- script.js
+++
@@ -1,8 +0,0 @@
-function showCertificate(certificateId) {
-    const certificate = document.getElementById(certificateId);
-    if (certificate.style.display === "none") {
-        certificate.style.display = "block";
-    } else {
-        certificate.style.display = "none";
-    }
-}
